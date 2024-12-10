3f0351af9d88914d4d21d1a40fbded5b1f15cebb
H A S A N <hasan25042019@gmail.com>
2024-12-10 21:44:54 +0600

    tailwind

--- live_class/test/test_project/src/App.jsx
+++ live_class/test/test_project/src/App.jsx
@@ -8,10 +8,16 @@ function App() {
 
   return (
     <>
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>   
-     </>
+      <form className='ml-5 p-4'>
+        <div>
+          <input type="text" placeholder='Name' className='m-2 p-1 mt-2 border border-gray-700 rounded-md hover:border-red-300 focus:ring-red-500 focus:ring-1 outline-none'/> 
+          <input type="text" placeholder='email' className='m-2 p-1 mt-2 border border-gray-700 rounded-md hover:border-red-300 focus:ring-red-500 focus:ring-1 outline-none'/> <br/>
+          <input type="text" placeholder='password' className='m-2 p-1 mt-2 border border-gray-700 rounded-md hover:border-red-300 focus:ring-red-500 focus:ring-1 outline-none'/> 
+          <input type="text" placeholder='confirm password' className='m-2 p-1 mt-2 border border-gray-700 rounded-md hover:border-red-300 focus:ring-red-500 focus:ring-1 outline-none'/> <br/>
+          <button className='ml-2 bg-green-400 p-1 hover:font-mono hover:bg-green-600 rounded-md'>Submit</button>
+        </div>
+      </form>
+    </>
   )
 }
 
