3c5d7eecb33eb79437e2c522425e0f651bde0344
H A S A N <hasan25042019@gmail.com>
2024-12-13 21:25:52 +0600

    tailwind package

---
+++ live_class/test/tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "selector",
+  theme: {
+    screens:{
+      tab: '640px',
+      desk: '1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
